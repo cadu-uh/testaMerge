07148edf408f6137c8aa467691f61ff5da715f58
Carlos Eduardo <carlos.eduardo1801@hotmail.com>
2021-06-11 17:08:37 -0300

    Segundo Commit

--- src/testemerge/TesteMerge.java
+++ src/testemerge/TesteMerge.java
@@ -12,7 +12,7 @@ public class TesteMerge {
     public static void main(String[] args) {
        /*Necessita-se fazer duas funcoes e tentar juntar elas*/
        int [] vet = new int[5];
-       vet = populaVet(vet);
+       //vet = populaVet(vet);
        
     }
 
