11bb4637065ed01455660aa3b1f27a44c9fc97e3
Carlos Eduardo <carlos.eduardo1801@hotmail.com>
2021-06-11 17:03:49 -0300

    Commit de merge para main

---
+++ src/testemerge/TesteMerge.java
@@ -0,0 +1,21 @@
+
+package testemerge;
+
+/**
+ *
+ * @author Carlos
+ */
+public class TesteMerge {
+  
+
+ 
+    public static void main(String[] args) {
+       /*Necessita-se fazer duas funcoes e tentar juntar elas*/
+       int [] vet = new int[5];
+       vet = populaVet(vet);
+       
+    }
+
+    
+    
+}
